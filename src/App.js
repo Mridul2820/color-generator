728a6888309a2f1144d2916c3783f2f717ccf056
Mridul2820 <immridulpanda2820@gmail.com>
2021-02-23 21:55:51 +0530

    form creation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
+import { useState } from 'react'
 import SingleColor from './components/SingleColor'
+import Values from 'values.js'
 import './App.scss';
 
 import React from 'react'
 
 const App = () => {
+    const [color, setColor] = useState('')
+    const [error, setError] = useState(false)
+    const [list, setList] = useState([])
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log('hello')
+    }
+
     return (
-        <div>
-            <h1>Get colors</h1>
-            <SingleColor />
-        </div>
+        <>
+            <div>
+                <section className="container">
+                    <h3>Color Generator</h3>
+                    <form onSubmit={handleSubmit} >
+                        <input 
+                            type="text" 
+                            value={color} 
+                            onChange={e => setColor(e.target.value)}
+                            placeholder="#008000 or Green"
+                        />
+                    </form>
+                    <button className="btn" type='submit'>Submit</button>
+                </section>
+                <section className="colors">
+                    <h4>color list</h4>
+                </section>
+            </div>
+        </>
     )
 }
 
